1d037e14e69e7a56f083744495c2c4cdfd32daa5
Helena Saborit <helena.sb91@gmail.com>
2023-01-27 18:44:23 +0100

    fixed issues with CSS in Studies section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { useState } from 'react';
 import Hobbies from './Hobbies';
 import { SiReact, SiJavascript, SiExpress, SiHandlebarsdotjs, SiMongodb, SiTailwindcss, SiPrestashop, SiTypescript, SiBabel, SiWhitesource } from 'react-icons/si'
-import { FaHtml5, FaCss3Alt, FaNodeJs, FaBootstrap, FaWordpress, FaGitSquare, FaGithub } from 'react-icons/fa'
+import { FaHtml5, FaCss3Alt, FaNodeJs, FaBootstrap, FaWordpress, FaGitSquare, FaGithub } from 'react-icons/fa';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function AboutMe(props) {
   
@@ -36,9 +39,8 @@ function AboutMe(props) {
   
   return (
     <div>
-      <div className="about">
-        
-        {/* <p>To hide this section OR to show as a pop up</p> */}
+      <div className="about">     
+
         <ul className="social">
                 {allData.socialLinks.map((el) => {
                     return (
@@ -48,121 +50,166 @@ function AboutMe(props) {
                     );
                   })}
         </ul>
+        
         <div>
-
-          <section className="personal-info">
-           
+          <section className="personal-info">         
             <div>
               <img src={allData.profilePic} alt="Helena's profile pic" style={profilePic}/>
-
             </div>
             <div className="about-me">
               <h2>ABOUT ME</h2>
               <p>{allData.aboutMe}</p>
-
             </div>
-                         
           </section>
         </div>
-      </div> 
-    <div className="about-else">
+
+      </div>
+
+      <div className="about-else blue-color">
   
-      <div className="blue-color" >
-        <section> 
-          <h3>SOFT SKILLS</h3>
-          {allData.softSkills.map((el) => {
-            return (
-              <div  className='skill-name' key={el.skillName}>
-                <li ><span style={skillTheme}>{el.skillName}</span></li>
-              </div>
-            
-            )})
-          }
-        </section>
-        <br />
+        {/* <div className="blue-color" > */}
+          <section> 
+            <h3>SOFT SKILLS</h3>
+            {allData.softSkills.map((el) => {
+              return (
+                <div  className='skill-name' key={el.skillName}>
+                  <li ><span style={skillTheme}>{el.skillName}</span></li>
+                </div>
+              )})
+            }
+          </section>
+          <br />
         
         <section>
           <h3>HARD SKILLS</h3>
-  
           <div> 
-          {/* <div className="twelve columns"> */}
             <ul className="hard-list">
               <li className="hard-li" title="Html5"><FaHtml5 style={{fontSize: "45px"}}/></li>
               <li className="hard-li" title="Css3"><FaCss3Alt style={{fontSize: "45px"}}/></li>
               <li className="hard-li" title="JavaScript"><SiJavascript style={{fontSize: "45px"}}/></li>
-              {/* <li className="hard-li" title="TypeScript"><SiTypescript style={{fontSize: "45px"}}/></li> */}
               <li className="hard-li" title="React"><SiReact style={{fontSize: "45px"}}/></li>
               <li className="hard-li" title="NodeJS"><FaNodeJs style={{fontSize: "45px"}}/></li>
               <li className="hard-li" title="ExpressJS"><SiExpress style={{fontSize: "45px"}}/></li>
               <li className="hard-li" title="MongoDB / Mongoose"><SiMongodb style={{fontSize: "45px"}}/></li>
-              {/* <li className="hard-li" title="Tailwinds"><SiTailwindcss style={{fontSize: "45px"}}/></li> */}
               <li className="hard-li" title="Handlebars"><SiHandlebarsdotjs style={{fontSize: "45px"}}/></li>
               <li className="hard-li" title="Bootstrap"><FaBootstrap style={{fontSize: "45px"}}/></li>
-              {/* <li className="hard-li" title="Git"><SiBabel style={{fontSize: "45px"}}/></li> */}
               <li className="hard-li" title="Github"><FaGithub style={{fontSize: "45px"}}/></li>
               <li className="hard-li" title="Git"><FaGitSquare style={{fontSize: "45px"}}/></li>
+              {/* <li className="hard-li" title="Git"><SiBabel style={{fontSize: "45px"}}/></li> */}
+              {/* <li className="hard-li" title="Tailwinds"><SiTailwindcss style={{fontSize: "45px"}}/></li> */}
+              {/* <li className="hard-li" title="TypeScript"><SiTypescript style={{fontSize: "45px"}}/></li> */}
               {/* <li className="hard-li" title="Wordpress"><FaWordpress style={{fontSize: "45px"}}/></li>
               <li className="hard-li" title="Prestashop"><SiPrestashop style={{fontSize: "45px"}}/></li> */}
             </ul>
           </div>
         </section>
 
-        
+      {/* </div> */}
       </div>
-        
-        <div className="blue-color studies">
+     {/* <div className="blue-color studies">
+       
           <section > 
-          <h3>STUDIES</h3>
-          {allData.studies.map((el) => {
-            return (
-              <div key={el.major}  >
-             
-                <li className='dark-blue-color'>
-                <div className="studies-title">
-                <p><b>{el.major}</b> at <i>{el.school}</i></p>
-                </div>
-                <div className='studies-descrp'>
-                <p className='dates'><i>Finished on {el.finishedAt}</i></p>
+            <h3>STUDIES</h3>
+            {allData.studies.map((el) => {
+                return (
+                  <div key={el.major}  >
+                
+                    <li className='dark-blue-color'>
+                    <div className="studies-title">
+                    <p><b>{el.major}</b> at <i>{el.school}</i></p>
+                    </div>
+                    <div className='studies-descrp'>
+                    <p className='dates'><i>Finished on {el.finishedAt}</i></p>
 
-                <p>{el.achivements}</p>
-                {el.certificate === "" 
-                ? <p></p>
-                : <p><a href={el.certificate} target="_blank">Show Certificate 🔗 </a> </p>
-                }
-                </div>
-                </li>
-              </div>
-            
-            )})
-          }
-          
+                    <p>{el.achivements}</p>
+                    {el.certificate === "" 
+                    ? <p></p>
+                    : <p><a href={el.certificate} target="_blank">Show Certificate 🔗 </a> </p>
+                    }
+                    </div>
+                    </li>
+                  </div>
+                
+                )})
+              }
           </section>
-          <br />
+         <br />
+         {/* </div>
+     
+         <div >
           <section > 
-          <h3>LANGUAGES</h3>
-          {allData.languages.map((el) => {
-            return (
-              <div key={el.languageName} className="languages"  >
-                <li ><b>{el.languageName}</b> <img src={el.arrow} alt={el.languageName} width={50}/> <i>{el.level}</i></li>
-              </div>
-            
-            )})
-          }
+            <h3>LANGUAGES</h3>
+            {allData.languages.map((el) => {
+              return (
+                <div key={el.languageName} className="languages"  >
+                  <li ><b>{el.languageName}</b> <img src={el.arrow} alt={el.languageName} width={50}/> <i>{el.level}</i></li>
+                </div>
+              
+              )})
+            }
         </section>
-        </div>
-          {/* <div>
+      </div>
+</div> */}
+
+<div className="about-else blue-color">
+      <Container>
+      <Row xs={1} md={2} lg={20}>
+        <Col>
+        <section > 
+            <h3>STUDIES</h3>
+            {allData.studies.map((el) => {
+                return (
+                  <div key={el.major}  >
+                
+                    <li className='dark-blue-color'>
+                    <div className="studies-title">
+                    <p><b>{el.major}</b> at <i>{el.school}</i></p>
+                    </div>
+                    <div className='studies-descrp'>
+                    <p className='dates'><i>Finished on {el.finishedAt}</i></p>
+
+                    <p>{el.achivements}</p>
+                    {el.certificate === "" 
+                    ? <p></p>
+                    : <p><a href={el.certificate} target="_blank">Show Certificate 🔗 </a> </p>
+                    }
+                    </div>
+                    </li>
+                  </div>
+                
+                )})
+              }
+          </section>
+        </Col>
+        <Col>
+        <section > 
+            <h3>LANGUAGES</h3>
+            {allData.languages.map((el) => {
+              return (
+                <div key={el.languageName} className="languages"  >
+                  <li ><b>{el.languageName}</b> <img src={el.arrow} alt={el.languageName} width={50}/> <i>{el.level}</i></li>
+                </div>
+              
+              )})
+            }
+        </section>
+        </Col>
+        {/* <Col>
+            <div>
           {listIsShowing === false
             ? <button className="hide-btn" onClick={toggleList} >Hobbies</button>
             : <div><button className="hide-btn" onClick={toggleList}>Hide Hobbies</button>
             <Hobbies data={allData} hideForm={toggleList} />
             </div>
         }
-          </div> */}
-        
+          </div> 
+        </Col> */}
+      </Row>
+      </Container>      
 
     </div>
       
-    </div>
+   </div>
   )
 }
 
